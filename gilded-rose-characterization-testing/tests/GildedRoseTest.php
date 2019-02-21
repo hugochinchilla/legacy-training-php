3b80e1eeb75f9519c29266e586d2ca73754b51ae
Hugo Chinchilla Carbonell <hchinchilla@habitissimo.com>
2019-02-21 09:38:45 +0100

    identified some characteristics

--- gilded-rose-characterization-testing/tests/GildedRoseTest.php
+++ gilded-rose-characterization-testing/tests/GildedRoseTest.php
@@ -8,9 +8,8 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-
     /** @test */
-    public function changeMe()
+    public function name_remains_unchanged()
     {
         /** @var Item[] $items */
         $items = array(new Item("aName", 10, 20));
@@ -19,5 +18,17 @@ class GildedRoseTest extends TestCase
         $gildedRose->update_quality();
 
         $this->assertEquals("aName", $items[0]->name);
+    }
+
+    /** @test */
+    public function sell_in_is_decremented_by_one()
+    {
+        /** @var Item[] $items */
+        $items = array(new Item("aName", 9, 20));
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->update_quality();
+
+        $this->assertEquals(8, $items[0]->sell_in);
     }
 }
